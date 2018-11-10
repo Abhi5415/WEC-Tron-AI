fad1bae3f17ed20d04a01842460981aee7f2bdcc
Shehryar Assad <xshehryar@gmail.com>
2018-11-09 19:35:59 -0500

    add starting code

--- src/Bike.java
+++ src/Bike.java
@@ -1,9 +1,9 @@
 public class Bike {
-    int x;
-    int y;
+    int row;
+    int col;
 
-    public Bike(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Bike(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
-public class Main implements Constants {
+public class Main {
     public static void main(String[] args) {
-        Bike s = new Bike(BIKE_1_X, BIKE_1_Y);
-        Bike p = new Bike(BIKE_2_X,BIKE_2_Y);
+        //
+        char[][] grid = new char[17][17];
+
+        for (int i = 0; i < 17; i++) {
+            grid[0][i] = 'w';
+            grid[i][0] = 'w';
+            grid[16][i] = 'w';
+            grid[i][16] = 'w';
+        }
+
+        // initial starting positions
+        Bike s = new Bike(8, 14);
+        Bike p = new Bike(8, 2);
 
 
-        Matrix m = new Matrix(MATRIX_ROWS, MATRIX_COLS, s, p);
+        grid[s.row][s.col] = 's';
+        grid[p.row][p.col] = 'p';
 
-        m.debug();
+
+
+        printGrid(grid);
+
+    }
+
+//    public static boolean distanceToNearestObstacle()
+
+    public static void printGrid(char[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                System.out.print(" " + grid[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
