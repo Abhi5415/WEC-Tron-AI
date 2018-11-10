f7b69c04583118e996a6508e1d20670e54481531
Shehryar Assad <xshehryar@gmail.com>
2018-11-09 22:59:26 -0500

    Finished genetic algorithm

--- src/Genome.java
+++ src/Genome.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 public class Genome {
     double closeFreespace;
     double closeWallDistance;
-    double[] closeRelativeEnemyX;
-    double[] closeRelativeEnemyY;
+    double[] closeRelativeEnemyX; // indexes S, L, R
+    double[]  closeRelativeEnemyY;
     double farFreespace;
     double farWallDistance;
     double[] farRelativeEnemyX;
@@ -12,8 +12,7 @@ public class Genome {
     char currDirection;
     boolean didWin;
 
-    public Genome() {
-        this.closeFreespace = random(-100, 100);
+    public Genome() {       this.closeFreespace = random(-100, 100);
         this.closeWallDistance = random(-100, 100);
         this.closeRelativeEnemyX = new double[3];
         this.closeRelativeEnemyX[0] = random(-100, 100);
@@ -69,23 +68,38 @@ public class Genome {
 
         char moveL = 's';
         char moveR = 's';
+        Bike bikeS = new Bike(0,0);
+        Bike bikeL = new Bike(0,0);
+        Bike bikeR = new Bike(0,0);
 
         switch (currentDirection) {
             case 'u':
                 moveL = 'l';
                 moveR = 'r';
+                bikeS = new Bike(myPosition.row - 1, myPosition.col);
+                bikeL = new Bike(myPosition.row, myPosition.col - 1);
+                bikeR = new Bike(myPosition.row, myPosition.col + 1);
                 break;
             case 'd':
                 moveL = 'r';
                 moveR = 'l';
+                bikeS = new Bike(myPosition.row + 1, myPosition.col);
+                bikeL = new Bike(myPosition.row, myPosition.col + 1);
+                bikeR = new Bike(myPosition.row, myPosition.col - 1);
                 break;
             case 'r':
                 moveL = 'u';
                 moveR = 'd';
+                bikeS = new Bike(myPosition.row, myPosition.col + 1);
+                bikeL = new Bike(myPosition.row - 1, myPosition.col);
+                bikeR = new Bike(myPosition.row + 1, myPosition.col);
                 break;
             case 'l':
                 moveL = 'd';
                 moveR = 'u';
+                bikeS = new Bike(myPosition.row, myPosition.col - 1);
+                bikeL = new Bike(myPosition.row + 1, myPosition.col);
+                bikeR = new Bike(myPosition.row - 1, myPosition.col);
                 break;
         }
 
@@ -93,14 +107,44 @@ public class Genome {
             ratingS += distanceToNearestObstacle(grid, myPosition, currentDirection) * farWallDistance;
             ratingL += distanceToNearestObstacle(grid, myPosition, moveL) * farWallDistance;
             ratingR += distanceToNearestObstacle(grid, myPosition, moveR) * farWallDistance;
+
+            ratingS += freeSpacesAtIndex(grid, bikeS) * farFreespace;
+            ratingL += freeSpacesAtIndex(grid, bikeL) * farFreespace;
+            ratingR += freeSpacesAtIndex(grid, bikeR) * farFreespace;
+
+            ratingS += relativeX(bikeS, enemyPosition) * farRelativeEnemyX[0];
+            ratingL += relativeX(bikeL, enemyPosition) * farRelativeEnemyX[1];
+            ratingR += relativeX(bikeR, enemyPosition) * farRelativeEnemyX[2];
+
+            ratingS += relativeY(bikeS, enemyPosition) * farRelativeEnemyY[0];
+            ratingL += relativeY(bikeL, enemyPosition) * farRelativeEnemyY[1];
+            ratingR += relativeY(bikeR, enemyPosition) * farRelativeEnemyY[2];
         } else {
             ratingS += distanceToNearestObstacle(grid, myPosition, currentDirection) * closeWallDistance;
             ratingL += distanceToNearestObstacle(grid, myPosition, moveL) * closeWallDistance;
             ratingR += distanceToNearestObstacle(grid, myPosition, moveR) * closeWallDistance;
+
+            ratingS += freeSpacesAtIndex(grid, bikeS) * closeFreespace;
+            ratingL += freeSpacesAtIndex(grid, bikeL) * closeFreespace;
+            ratingR += freeSpacesAtIndex(grid, bikeR) * closeFreespace;
+
+            ratingS += relativeX(bikeS, enemyPosition) * closeRelativeEnemyX[0];
+            ratingL += relativeX(bikeL, enemyPosition) * closeRelativeEnemyX[1];
+            ratingR += relativeX(bikeR, enemyPosition) * closeRelativeEnemyX[2];
+
+            ratingS += relativeY(bikeS, enemyPosition) * closeRelativeEnemyY[0];
+            ratingL += relativeY(bikeL, enemyPosition) * closeRelativeEnemyY[1];
+            ratingR += relativeY(bikeR, enemyPosition) * closeRelativeEnemyY[2];
         }
 
-        
-
+        // return based on highest rating values
+        if (ratingS >= ratingL && ratingS >= ratingR) {
+            return 's';
+        } else if (ratingL >= ratingS && ratingL >= ratingR) {
+            return 'l';
+        } else {
+            return 'r';
+        }
     }
 
     /*
@@ -140,7 +184,7 @@ Calculates the distance to the nearest obstacle (a wall or another player) given
     /*
     Calculates the free spaces in a 3x3 square with the center as the Bike passed.
      */
-    public int freeSpacesAtIndex(Bike b, char[][] state) {
+    public int freeSpacesAtIndex(char[][] state, Bike b) {
         int x = b.row;
         int y = b.col;
 
